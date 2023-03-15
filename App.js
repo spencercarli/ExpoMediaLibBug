773a06546b4acb3e200db6eb4f75afcf43f8d4d5
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2023-03-15 14:18:40 -0400

    Setup media library

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as MediaLibrary from "expo-media-library";
 
 export default function App() {
+  const [mediaLibraryPermission, requestMediaLibraryPermission] =
+    MediaLibrary.usePermissions();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{mediaLibraryPermission?.status}</Text>
+      <Button
+        title="Request Media Library Permissions"
+        onPress={requestMediaLibraryPermission}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
